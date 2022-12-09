7ac61ea6131052ac5b95235d7fea83b292497f0c
Yacov Kopel <kokokopel94@gmail.com>
2022-12-08 22:20:16 -0800

    added README content and notes

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # EZ-Scheduler
+
+## Description
+
+This webpage was made using HTML, CSS, and Javascript. The purpose of this webpage is to allow users to input daily tasks into a hourly schedule that indicates present past and future "TO-DO's.The color scheme indicates that gray is hours that have past, red is the current hour and green is future hours.
+
+## Installation
+
+N/A
+
+## Usage
+
+To use this page, the user can press on any hour displayed and enter Text in any time slot. By pressing on the floppy-disk emoji, the text will be saved in local storage and will continue to populate that text area until the user deletes or replaces the text with new text. 
+   ![Screenshot of webpage](./assets/images/EZ-Scheduler-Img.jpeg)
+
+## Credits
+
+N/A
+
+## License
+
+N/A
--- assets/script.js
+++ assets/script.js
@@ -10,6 +10,7 @@ var fourteen= $("#hour-14");
 var fifteen= $("#hour-15");
 var sixteen= $("#hour-16");
 var seventeen= $("#hour-17");
+// array of all the 'id's
 var alltimes = [
   six,
   seven,
@@ -25,6 +26,7 @@ var alltimes = [
   seventeen,
 ];
 
+// trimmed version of Ids to get time string
 var alltimesNumber = [
   six.text().trim(),
   seven.text().trim(),
@@ -40,37 +42,37 @@ var alltimesNumber = [
   seventeen.text().trim(),
 ];
 
-var container= $(".container-lg")
-
 var time = dayjs().format('HA');
-  $('#time').text(time);
 
-  var numberTimeArray = [];
-
+var numberTimeArray = [];
+// For loop that appends just the digits of the time string in to a new array
 for (var i = 0; i < alltimesNumber.length; i++) {
   let element = alltimesNumber[i];
   if (element.length === 3) numberTimeArray.push(element[0]);
   if (element.length === 4) numberTimeArray.push(element[0] + element[1]);
 }
 
-// if (time.length === 3) time = time[0];
-// if (time.length === 4) time = time[0] + time[1];
-
+// turns time from string to number
 time = parseInt(time);
 
+// loops through array of digits and compares to current time to assign past, present, or future class
 for (var index = 0; index < numberTimeArray.length; index++) {
   let number = parseInt(numberTimeArray[index]);
   if (time === number) alltimes[index].addClass('present');
   if (time < number) alltimes[index].addClass('future');
   if (time > number) alltimes[index].addClass('past');
 }
+
+// sets key and value pair for items in local storage
 function saveInput() {
   var userInput = $(this).siblings('.description').val();
-  var key = $(this).parent().attr('id');
-  localStorage.setItem(key, userInput);
+  var hourId = $(this).parent().attr('id');
+  localStorage.setItem(hourId, userInput);
 }
+// A listener for click events on the save button that saves text into local storage
 $('.saveBtn').on('click', saveInput);
 
+// retrieves items from local storage to always be displayed on-screen at specific hour specified by id
 function renderInput(){
   for (let i = 0; i < alltimes.length; i++) {
     var key = alltimes[i].attr('id');
@@ -79,34 +81,15 @@ function renderInput(){
     
   }
 }
-// function saveInput(event){
-// event.preventDefault();
-// localStorage.setItem($(event.target).attr('id'),
-// $(event.target).siblings('textarea')
-// }
 
-//   localStorage.setItem("studentGrade", JSON.stringify(studentGrade));
-// });
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+// displays the current date in the header of the page
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-
-  
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
   var currentDay = dayjs().format('dddd[ - ] MMM DD, YYYY');
 $('#currentDay').text(currentDay);
+setInterval(() => {
+  var liveTime=dayjs().format('H:mm A');
+  $('#time').text(liveTime);
+}, 1000);
 });
+
  renderInput()
